d9182e767799d70cb635bc704285ef842b89bf35
Caio Bello <122856066+caiobzm@users.noreply.github.com>
2023-03-27 03:53:37 -0300

    Add files via upload

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"GerenciadorDeVendas","l":"GerenciadorDeVendas()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"GerenciadorDeVendas","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
